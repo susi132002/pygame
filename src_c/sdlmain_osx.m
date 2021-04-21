67134995e4d6246fb060f1a8bd45d4f9f0aa20ff
Starbuck5 <charlie.c.hayden@gmail.com>
2021-04-20 23:33:32 -0700

    Revert some sdlmain_osx.m changes that broke python2

--- src_c/sdlmain_osx.m
+++ src_c/sdlmain_osx.m
@@ -396,6 +396,8 @@ static PyMethodDef macosx_builtins[] =
 
 MODINIT_DEFINE (sdlmain_osx)
 {
+    PyObject *module;
+
     /* create the module */
 #if PY3
     static struct PyModuleDef _module = {
@@ -418,8 +420,10 @@ MODINIT_DEFINE (sdlmain_osx)
     }
 
 #if PY3
-    MODINIT_RETURN(PyModule_Create(&_module));
+    module = PyModule_Create(&_module);
 #else
-    MODINIT_RETURN(Py_InitModule3(MODPREFIX "sdlmain_osx", macosx_builtins, NULL));
+    module = Py_InitModule3(MODPREFIX "sdlmain_osx", macosx_builtins, NULL);
 #endif
+
+MODINIT_RETURN(module);
 }
